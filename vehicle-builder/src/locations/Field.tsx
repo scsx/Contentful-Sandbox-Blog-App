f9f20128da3e6f3bf0dddd0db168d5f52a1c259b
Jorge Soucasaux Monteiro <jorge.monteiro@vodafone.com>
2026-02-26 18:20:32 +0000

    flex

--- vehicle-builder/src/locations/Field.tsx
+++ vehicle-builder/src/locations/Field.tsx
@@ -1,18 +1,24 @@
-import { Paragraph } from '@contentful/f36-components';
-import { FieldAppSDK } from '@contentful/app-sdk';
-import { useSDK } from '@contentful/react-apps-toolkit';
-import { Tabs } from '@contentful/f36-components';
-import { Select, FormControl } from '@contentful/f36-components';
-import { useState, useEffect } from 'react';
+import {
+  Flex,
+  Box,
+  Paragraph,
+  Heading,
+  Tabs,
+  Select,
+  FormControl
+} from '@contentful/f36-components'
+import { FieldAppSDK } from '@contentful/app-sdk'
+import { useSDK } from '@contentful/react-apps-toolkit'
+import { useState, useEffect } from 'react'
 
 const Field = () => {
-  const sdk = useSDK<FieldAppSDK>();
-  const [vehicleType, setVehicleType] = useState('car');
-  const [fuelType, setFuelType] = useState('petrol');
+  const sdk = useSDK<FieldAppSDK>()
+  const [vehicleType, setVehicleType] = useState('car')
+  const [fuelType, setFuelType] = useState('petrol')
 
   useEffect(() => {
-  sdk.window.startAutoResizer();
-}, [sdk.window]);
+    sdk.window.startAutoResizer()
+  }, [sdk.window])
 
   /*
      To use the cma, access it as follows.
@@ -24,57 +30,54 @@ const Field = () => {
   // -> https://www.contentful.com/developers/docs/extensibility/field-editors/
   return (
     <>
-    <Paragraph>Hello Entry Field Component (from field) (AppId: {sdk.ids.app})</Paragraph>
+      <Flex justifyContent='space-between' alignItems='start'>
+        <Box flexGrow={1}>
+          <FormControl>
+            <FormControl.Label>Vehicle Type</FormControl.Label>
 
-    <FormControl>
-        <FormControl.Label>Vehicle Type</FormControl.Label>
+            <Select value={vehicleType} onChange={(e) => setVehicleType(e.target.value)}>
+              <Select.Option value='car'>Car</Select.Option>
+              <Select.Option value='boat'>Boat</Select.Option>
+              <Select.Option value='bus'>Bus</Select.Option>
+            </Select>
+          </FormControl>
 
-        <Select
-          value={vehicleType}
-          onChange={(e) => setVehicleType(e.target.value)}
-        >
-          <Select.Option value="car">Car</Select.Option>
-          <Select.Option value="boat">Boat</Select.Option>
-          <Select.Option value="bus">Bus</Select.Option>
-        </Select>
-      </FormControl>
+          {vehicleType === 'car' && (
+            <FormControl style={{ marginTop: '16px' }}>
+              <FormControl.Label>Fuel Type</FormControl.Label>
 
-      {vehicleType === 'car' && (
-        <FormControl style={{ marginTop: '16px' }}>
-          <FormControl.Label>Fuel Type</FormControl.Label>
+              <Select value={fuelType} onChange={(e) => setFuelType(e.target.value)}>
+                <Select.Option value='petrol'>Petrol</Select.Option>
+                <Select.Option value='diesel'>Diesel</Select.Option>
+                <Select.Option value='electric'>Electric</Select.Option>
+              </Select>
+            </FormControl>
+          )}
 
-          <Select
-            value={fuelType}
-            onChange={(e) => setFuelType(e.target.value)}
-          >
-            <Select.Option value="petrol">Petrol</Select.Option>
-            <Select.Option value="diesel">Diesel</Select.Option>
-            <Select.Option value="electric">Electric</Select.Option>
-          </Select>
-        </FormControl>
-      )}
+          <Tabs>
+            <Tabs.List>
+              <Tabs.Tab panelId='basic'>Basic</Tabs.Tab>
+              <Tabs.Tab panelId='engine'>Engine</Tabs.Tab>
+              <Tabs.Tab panelId='legal'>Legal</Tabs.Tab>
+            </Tabs.List>
 
-     <Tabs>
-        <Tabs.List>
-          <Tabs.Tab panelId="basic" >Basic</Tabs.Tab>
-          <Tabs.Tab panelId="engine">Engine</Tabs.Tab>
-          <Tabs.Tab panelId="legal">Legal</Tabs.Tab>
-        </Tabs.List>
+            <Tabs.Panel id='basic'>Basic info content</Tabs.Panel>
 
-        <Tabs.Panel id="basic">
-          Basic info content
-        </Tabs.Panel>
+            <Tabs.Panel id='engine'>Engine content</Tabs.Panel>
 
-        <Tabs.Panel id="engine">
-          Engine content
-        </Tabs.Panel>
-
-        <Tabs.Panel id="legal">
-          Legal content
-        </Tabs.Panel>
-      </Tabs>
+            <Tabs.Panel id='legal'>Legal content</Tabs.Panel>
+          </Tabs>
+        </Box>
+        <Box>
+          <Heading fontSize='fontSizeXl'>Final price</Heading>
+          <Heading fontSize='fontSize3Xl' fontColor='green500'>
+            55€/day
+          </Heading>
+        </Box>
+      </Flex>
+      <Paragraph>Hello Entry Field Component (from field) (AppId: {sdk.ids.app})</Paragraph>
     </>
   )
-};
+}
 
-export default Field;
+export default Field
