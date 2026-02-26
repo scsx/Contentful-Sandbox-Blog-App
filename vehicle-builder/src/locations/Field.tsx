5bd032689dc58296499f45eb1e68ea86d3631430
Jorge Soucasaux Monteiro <jorge.monteiro@vodafone.com>
2026-02-26 17:55:36 +0000

    height

--- vehicle-builder/src/locations/Field.tsx
+++ vehicle-builder/src/locations/Field.tsx
@@ -3,11 +3,17 @@ import { FieldAppSDK } from '@contentful/app-sdk';
 import { useSDK } from '@contentful/react-apps-toolkit';
 import { Tabs } from '@contentful/f36-components';
 import { Select, FormControl } from '@contentful/f36-components';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const Field = () => {
   const sdk = useSDK<FieldAppSDK>();
   const [vehicleType, setVehicleType] = useState('car');
+  const [fuelType, setFuelType] = useState('petrol');
+
+  useEffect(() => {
+  sdk.window.startAutoResizer();
+}, [sdk.window]);
+
   /*
      To use the cma, access it as follows.
      If it is not needed, you can remove the next line.
@@ -21,18 +27,33 @@ const Field = () => {
     <Paragraph>Hello Entry Field Component (from field) (AppId: {sdk.ids.app})</Paragraph>
 
     <FormControl>
-      <FormControl.Label>Vehicle Type</FormControl.Label>
+        <FormControl.Label>Vehicle Type</FormControl.Label>
 
-      <Select
-        value={vehicleType}
-        onChange={(e) => setVehicleType(e.target.value)}
-      >
-        <Select.Option value="car">Car</Select.Option>
-        <Select.Option value="boat">Boat</Select.Option>
-        <Select.Option value="bus">Bus</Select.Option>
-      </Select>
-    </FormControl>
-    
+        <Select
+          value={vehicleType}
+          onChange={(e) => setVehicleType(e.target.value)}
+        >
+          <Select.Option value="car">Car</Select.Option>
+          <Select.Option value="boat">Boat</Select.Option>
+          <Select.Option value="bus">Bus</Select.Option>
+        </Select>
+      </FormControl>
+
+      {vehicleType === 'car' && (
+        <FormControl style={{ marginTop: '16px' }}>
+          <FormControl.Label>Fuel Type</FormControl.Label>
+
+          <Select
+            value={fuelType}
+            onChange={(e) => setFuelType(e.target.value)}
+          >
+            <Select.Option value="petrol">Petrol</Select.Option>
+            <Select.Option value="diesel">Diesel</Select.Option>
+            <Select.Option value="electric">Electric</Select.Option>
+          </Select>
+        </FormControl>
+      )}
+
      <Tabs>
         <Tabs.List>
           <Tabs.Tab panelId="basic" >Basic</Tabs.Tab>
