af16002a651488999693b5c3b2af19a2abbe769a
Jorge Soucasaux Monteiro <jorge.monteiro@vodafone.com>
2026-02-26 17:46:05 +0000

    UI start

--- vehicle-builder/src/locations/Field.tsx
+++ vehicle-builder/src/locations/Field.tsx
@@ -1,9 +1,13 @@
 import { Paragraph } from '@contentful/f36-components';
 import { FieldAppSDK } from '@contentful/app-sdk';
 import { useSDK } from '@contentful/react-apps-toolkit';
+import { Tabs } from '@contentful/f36-components';
+import { Select, FormControl } from '@contentful/f36-components';
+import { useState } from 'react';
 
 const Field = () => {
   const sdk = useSDK<FieldAppSDK>();
+  const [vehicleType, setVehicleType] = useState('car');
   /*
      To use the cma, access it as follows.
      If it is not needed, you can remove the next line.
@@ -12,7 +16,44 @@ const Field = () => {
   // If you only want to extend Contentful's default editing experience
   // reuse Contentful's editor components
   // -> https://www.contentful.com/developers/docs/extensibility/field-editors/
-  return <Paragraph>Hello Entry Field Component (AppId: {sdk.ids.app})</Paragraph>;
+  return (
+    <>
+    <Paragraph>Hello Entry Field Component (from field) (AppId: {sdk.ids.app})</Paragraph>
+
+    <FormControl>
+      <FormControl.Label>Vehicle Type</FormControl.Label>
+
+      <Select
+        value={vehicleType}
+        onChange={(e) => setVehicleType(e.target.value)}
+      >
+        <Select.Option value="car">Car</Select.Option>
+        <Select.Option value="boat">Boat</Select.Option>
+        <Select.Option value="bus">Bus</Select.Option>
+      </Select>
+    </FormControl>
+    
+     <Tabs>
+        <Tabs.List>
+          <Tabs.Tab panelId="basic" >Basic</Tabs.Tab>
+          <Tabs.Tab panelId="engine">Engine</Tabs.Tab>
+          <Tabs.Tab panelId="legal">Legal</Tabs.Tab>
+        </Tabs.List>
+
+        <Tabs.Panel id="basic">
+          Basic info content
+        </Tabs.Panel>
+
+        <Tabs.Panel id="engine">
+          Engine content
+        </Tabs.Panel>
+
+        <Tabs.Panel id="legal">
+          Legal content
+        </Tabs.Panel>
+      </Tabs>
+    </>
+  )
 };
 
 export default Field;
